56094bbe905caf083a281979bcf4679bb91c441c
Shawn Kebel <fxrender@outlook.com>
2022-11-13 02:59:30 -0600

    :sparkles: Create `Shopping Categories` design

--- src/data.js
+++ src/data.js
@@ -15,8 +15,7 @@ export const sliderItems = [
   },
   {
     id: 3,
-    img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f09a2538-8869-4e67-b73e-47411239ec93/d99bn0n-8e9734dc-ac42-4be8-a832-161f607903f6.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2YwOWEyNTM4LTg4NjktNGU2Ny1iNzNlLTQ3NDExMjM5ZWM5M1wvZDk5Ym4wbi04ZTk3MzRkYy1hYzQyLTRiZTgtYTgzMi0xNjFmNjA3OTAzZjYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0._3qC-wgrwE6_i-riXRGeZZBs1PzOAhB9GB7e0eRZUVM",
-    // img: "http://www.pngmart.com/files/1/Mens-Fashion.png",
+    img: "http://www.pngmart.com/files/1/Mens-Fashion.png",
     title: "MEN'S CASUALS",
     desc: "Don't compromise on style! Get a flat 30% off new arrivals.",
     bg: "f5fafd",
@@ -26,54 +25,17 @@ export const sliderItems = [
 export const categories = [
   {
     id: 1,
-    img: "http://purepng.com/public/uploads/large/purepng.com-fashion-girlwomenpeoplepersonsfemalefashion-1121525113646zapsm.png",
-    title: "DRESS STYLE!",
+    img: "https://www.sex-dresses.com/wp-content/uploads/2016/04/Women-Zip-Front-Style-Sleeveless-Denim-Dress--600x900.jpg",
+    title: "DRESS STYLE",
   },
   {
     id: 2,
-    // img: "https://www.action.com/globalassets/cmsarticleimages/96/54/2555634_5411504136174-110.png/",
-    img: "https://officialpsds.com/imageview/r8/ln/r8ln05_large.png?1528122502",
-    title: "LOUNGEWEAR LOVE",
+    img: "https://www.bagsher.com/product_images/n/867/Autumn-Pajamas-Set-Ladies-Vintage-Pajamas-For-Women-Sleepwear-Femmes__00797_zoom.jpg",
+    title: "LOUNGEWEAR",
   },
   {
     id: 3,
-    // img: "https://www.pngarts.com/files/1/Women-Leather-Jacket-Transparent-Background-PNG.png",
-    img: "https://i.pinimg.com/originals/c9/c2/56/c9c256ed84f74b6d82247f68a88ed3ec.png",
+    img: "https://happygentleman.com/wp-content/uploads/2019/11/brandos-mens-leather-jacket-timber4.jpg",
     title: "LIGHT JACKETS",
-  },
-];
-
-export const popularProducts = [
-  {
-    id: 1,
-    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
-  },
-  {
-    id: 2,
-    img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
-  },
-  {
-    id: 3,
-    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
-  },
-  {
-    id: 4,
-    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
-  },
-  {
-    id: 5,
-    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
-  },
-  {
-    id: 6,
-    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
-  },
-  {
-    id: 7,
-    img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
-  },
-  {
-    id: 8,
-    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
   },
 ];
