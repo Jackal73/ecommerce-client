17784ef9f5b5b1278f33f7cc09799479cd8f5365
Shawn Kebel <fxrender@outlook.com>
2022-11-13 01:56:27 -0600

    :sparkles: Add `Announcement` styled component

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
 
 const Announcement = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <Container>Super Deal! Free Shipping on Orders Over $100</Container>;
+};
 
-export default Announcement
+export default Announcement;
